17352bb2818ac7a718bc6d5f0df3d2bedba4686b
sashank chavaly <sashank.chavaly@academy.com>
2023-05-21 22:29:11 +0530

    book my show

---
+++ bookmyshow/src/config/mysql.js
@@ -0,0 +1,19 @@
+const Sequalize = require("sequelize");
+console.log(process.env.PASSWORD)
+// setup a connection
+const sequalize = new Sequalize(process.env.DB_NAME, process.env.USER_NAME, process.env.PASSWORD, {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+// Connection check
+const connectToDB = async () => {
+  try {
+    await sequalize.authenticate();
+    console.log("Connected to database successfully!");
+  } catch (err) {
+    console.log("There was an error to connect DB", err);
+  }
+};
+
+module.exports = {sequalize, connectToDB}
